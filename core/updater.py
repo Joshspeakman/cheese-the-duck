c3b41c2674c64c73adff93e5f130dbc89b9dd460
Josh Speakman <joshspeakman@example.com>
2026-01-08 14:45:25 -0800

    v1.4.0: Bug fixes and improvements
    - Fix menu action handlers
    - Stability improvements

--- core/updater.py
+++ core/updater.py
@@ -25,7 +25,7 @@ from config import GAME_DIR, SAVE_DIR
 
 
 # Game version - Update this when releasing new versions
-GAME_VERSION = "1.3.8"
+GAME_VERSION = "1.4.0"
 
 # GitHub repository info
 GITHUB_OWNER = "Joshspeakman"
