10e86457a873c257e7c9f1893c04fefe49689b08
Josh Speakman <joshspeakman@example.com>
2026-02-10 10:18:31 -0800

    Update GAME_VERSION to 2.1.1

--- core/updater.py
+++ core/updater.py
@@ -25,7 +25,7 @@ from config import GAME_DIR, SAVE_DIR
 
 
 # Game version - Update this when releasing new versions
-GAME_VERSION = "1.8.0"
+GAME_VERSION = "2.1.1"
 
 # GitHub repository info
 GITHUB_OWNER = "Joshspeakman"
